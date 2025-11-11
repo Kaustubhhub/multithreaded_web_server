3fe9617d72d999797c94404aec9fb8958bf019d9
kaustubh <kaustubhkumbhare02@gmail.com>
2025-11-11 19:44:41 +0000

    single threaded

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::{
+    fs,
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+
+    let (status_line, file_name) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 ok", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let content = fs::read_to_string(file_name).unwrap();
+    let length = content.len();
+
+    let reponse = format!("{status_line}\r\nContent-Lenght:{length}\r\n\r\n{content}");
+
+    stream.write_all(reponse.as_bytes()).unwrap();
 }
